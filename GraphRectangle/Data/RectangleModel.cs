f03fe9298902e6789fe50e51ef5912c8d735104a
Gean Kyle Leonor <geankyle@terraschwartz.com>
2022-08-09 10:08:27 +0800

    Initial GraphService

---
+++ GraphRectangle/Data/RectangleModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GraphRectangle.Data
+{
+    public class RectangleModel
+    {
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+        public int PointX2 { get; set; }
+        public int PointY2 { get; set; }
+        public bool Conflict { get; set; } = false;
+
+    }
+}
---
+++ GraphRectangle/Service/GraphService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphRectangle.Service
+{
+    public class GraphService
+    {
+        private readonly Graphics _graphics;
+        private readonly int _height;
+        private readonly int _width;
+
+        public GraphService(Graphics graphics, int height, int width)
+        {
+            _graphics = graphics;
+            _height = height;
+            _width = width;
+        }
+
+        /// <summary>
+        /// Generate X and Y axis line graph
+        /// </summary>
+        /// <param name="x">x axis</param>
+        /// <param name="y">y axis</param>
+        public void GenerateGraph(int x, int y)
+        {
+
+        }
+    }
+}
